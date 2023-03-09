0b0e97102a73e4528157a300a50c4b57431f3dd5
alkrosr <aleksey.s.krotenko@gmail.com>
2023-03-09 14:23:54 +0500

    initial commit with firebase and vuetify

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { db } from '@/firebase/config'
+import { query, where, collection, getDocs } from 'firebase/firestore'
+
+onMounted(async () => {
+  const q = query(collection(db, 'songs'))
+
+  const querySnapshot = await getDocs(q)
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, ' => ', doc.data())
+  })
+})
+</script>
+
+<template>
+  <main></main>
+</template>
+
+<style scoped></style>
