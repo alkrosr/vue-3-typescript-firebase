6451d06c7d4b0b74fc5d5112fe38b1ada7648995
alkrosr <aleksey.s.krotenko@gmail.com>
2023-03-09 17:21:56 +0500

    add to favorite event - song

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,45 @@ import SongsPlayer from '@/components/SongsPlayer.vue'
 
 const songs = ref<Song[]>([])
 
+const playingSongs: Song[] = [
+  {
+    id: '1',
+    title: 'So What',
+    artist: 'Miles Davis',
+    year: 1959,
+  },
+  {
+    id: '2',
+    title: 'Smells Like Teen Spirit',
+    artist: 'Nirvana',
+    year: 1991,
+  },
+  {
+    id: '3',
+    title: 'What a Wonderful World',
+    artist: 'Louis  Armstrong',
+    year: 1967,
+  },
+  {
+    id: '4',
+    title: 'The Look',
+    artist: 'Roxette',
+    year: 1988,
+  },
+]
+
 onMounted(async () => {
   getSongs(songs)
 })
+
+const addToFavorites = (id: string) => {
+  console.log(id)
+}
 </script>
 
 <template>
   <main>
-    <SongsPlayer />
+    <SongsPlayer @add-to-favorite="addToFavorites" :songs="playingSongs" />
     <SongsList :songs="songs" />
   </main>
 </template>
